8be0040ee7ceb2be2f8fc3c620e50081a1faedae
tracer-NEMU2020 <chentju@qq.com>
2020-09-29 03:34:36 -0700

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:34:36 up  6:37,  1 user,  load average: 0.01, 0.03, 0.00 6c262b0d8d622337b14e8309ed325c797f4f9d6a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -74,10 +74,10 @@ static bool make_token(char *e) {
         /* Try all rules one by one. */
         for (i = 0; i < NR_REGEX; i++) {
             if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-                printf("%d\n", rules[i].token_type == '-' );
-                printf("%d\n", *(e + position + 1) - '0' );
+//                printf("%d\n", rules[i].token_type == '-' );
+//                printf("%d\n", *(e + position + 1) - '0' );
 //                printf("%c\n", (e + position + 1) - '0' >= 0 && (e + position + 1) - '9' <= 0 );
-                if (rules[i].token_type == '-' && (e + position + 1) - '0' >= 0 && (e + position + 1) - '9' <= 0 &&
+                if (rules[i].token_type == '-' && *(e + position + 1) - '0' >= 0 && *(e + position + 1) - '9' <= 0 &&
                     (!i || tokens[i - 1].type != NUM)) {
                     printf("%s",(e + position + 1));
                     continue;
