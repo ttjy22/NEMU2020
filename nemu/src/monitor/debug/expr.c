38d29915fabe89b045f9db8accbe41589f62029b
tracer-NEMU2020 <chentju@qq.com>
2020-09-29 05:39:57 -0700

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:39:57 up  8:42,  1 user,  load average: 0.10, 0.13, 0.05 a6eaff59919ff4102515573974ba0a44bfa87a68

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -89,8 +89,8 @@ static bool make_token(char *e) {
                 char *substr_start = e + position;
                 int substr_len = pmatch.rm_eo;
 
-                Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position,
-                    substr_len, substr_len, substr_start);
+//                Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position,
+//                    substr_len, substr_len, substr_start);
                 position += substr_len;
 
                 /* TODO: Now a new token is recognized with rules[i]. Add codes
@@ -105,10 +105,9 @@ static bool make_token(char *e) {
                         break;
                     default:
                         tokens[nr_token].type = rules[i].token_type;
-                        if (tokens[nr_token].type != '(' && tokens[nr_token].type != ')'&& tokens[nr_token].type != NOTYPE)
-                        {
+                        if (tokens[nr_token].type != '(' && tokens[nr_token].type != ')' &&
+                            tokens[nr_token].type != NOTYPE) {
                             strcpy(tokens[nr_token].str, "OP");
-                            puts("OP");
                         }
                 }
 //                Log("%d", tokens[nr_token].type);
