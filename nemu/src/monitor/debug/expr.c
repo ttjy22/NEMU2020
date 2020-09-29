c688dadc74a24cc41e1c37993bf4d2f578cda226
tracer-NEMU2020 <chentju@qq.com>
2020-09-29 04:10:08 -0700

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:10:08 up  7:12,  1 user,  load average: 0.11, 0.06, 0.02 bb664befb005ca67e73f85de1f718834e979f480

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -34,8 +34,8 @@ static struct rule {
         {"(0|-?[1-9][0-9]*)", NUM},                    // plus
         {"!",                 NOT},                     // equal
         {"&&",                AND},                     // equal
-//        {"||",                OR},
-//        {"*",                 DEREF},                       // equal
+        {"||",                OR},
+        {"\\*",                 DEREF},                       // equal
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
