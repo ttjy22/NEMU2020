8e80c4ff0e3b3c25d21eebb156540615500ffea0
tracer-NEMU2020 <chentju@qq.com>
2020-09-29 02:45:43 -0700

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:45:43 up  5:48,  1 user,  load average: 0.18, 0.07, 0.02 67020adc6056ef5f6dc43da224725a95a6a68f6b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -69,7 +69,7 @@ static int count() {
             else if (tokens[i].type == ')') {
                 while (t_op&&stk_op[t_op--] != '(') {
                     int tp = stk_op[t_op + 1];
-                    printf("stk_op:    %d\n", tp);
+//                    printf("stk_op:    %d\n", tp);
                     int a = stk_n[t_n--], b = stk_n[t_n];
                     if (tp == '+')stk_n[t_n] = a + b;
                     if (tp == '-')stk_n[t_n] = b - a;
@@ -90,7 +90,7 @@ static int count() {
             }
         } else {
             stk_n[++t_n] = atoi(tokens[i].str);
-            printf("stk_n:    %d\n", stk_n[t_n]);
+//            printf("stk_n:    %d\n", stk_n[t_n]);
         }
     }
     while (t_op) {
