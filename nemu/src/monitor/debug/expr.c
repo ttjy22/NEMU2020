ab1555684af19f19cc53c1fbdf46b47b9d718100
tracer-NEMU2020 <chentju@qq.com>
2020-09-29 03:21:54 -0700

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:21:54 up  6:24,  1 user,  load average: 0.02, 0.02, 0.00 27df3f76f8c34e1aec386f9512a0f849d35988

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -74,7 +74,7 @@ static bool make_token(char *e) {
         /* Try all rules one by one. */
         for (i = 0; i < NR_REGEX; i++) {
             if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-                if (rules[i].token_type == '-' && e[position + 1] >= '0' && e[position + 1] <= '9' &&
+                if (rules[i].token_type == '-' && (e + position + 1) - '0' >= 0 && (e + position + 1) - '9' <= 0 &&
                     (!i || tokens[i - 1].type != NUM)) {
                     printf("hi\nq");
                     continue;
