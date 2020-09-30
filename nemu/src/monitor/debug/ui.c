69e45eb9d9237fa603d695063cce9ad9501b62e9
tracer-NEMU2020 <chentju@qq.com>
2020-09-30 04:11:16 -0700

    >  compile NEMU 3018001704 ttjy Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:11:16 up 18:39,  1 user,  load average: 0.03, 0.06, 0.02 d686a45a4ad1501c4b0f457bbdb1b07fd6a5ae

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -116,9 +116,11 @@ static int count() {
                     if (!strcmp("$ebx", tokens[i].str))stk_n[++t_n] = cpu.ebx,printf("%d\n",cpu.ebx);
                     if (!strcmp("$edi", tokens[i].str))stk_n[++t_n] = cpu.edi,printf("%d\n",cpu.edi);
                     if (!strcmp("$eip", tokens[i].str))stk_n[++t_n] = cpu.eip, printf("%d\n", cpu.eip);
+                    puts("----------");
                     printf("%d\n", strcmp("$eip", tokens[i].str));
                     printf("%s\n", tokens[i].str);
                     printf("%d\n", stk_n[t_n]);
+                    puts("----------");
                 } else if (tp == NOT) {
                     stk_n[++t_n] = !atoi(tokens[++i].str);
                 } else stk_op[++t_op] = tp;
